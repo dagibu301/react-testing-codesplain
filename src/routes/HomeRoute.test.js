5a5b30e99df9e790733140259acf659ed091a75d
David Giraldo <davidgiraldobueno@gmail.com>
2024-07-10 22:57:50 -0500

    test async code

---
+++ src/routes/HomeRoute.test.js
@@ -0,0 +1,59 @@
+import { render, screen } from "@testing-library/react";
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+import { MemoryRouter } from "react-router-dom";
+import HomeRoute from "./HomeRoute";
+
+const handlers = [
+    rest.get('/api/repositories', (req, res, ctx) => {
+        const language = req.url.searchParams.get('q').split('language:')[1];
+        return res(
+            ctx.json({
+                items: [
+                    {
+                        id: 1,
+                        full_name: `${language}_1`,
+                    },
+                    {
+                        id: 2,
+                        full_name: `${language}_2`,
+                    },
+                ]
+            })
+        )
+    })];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('Home Route component', () => {
+    it('should render two links for each language', async () => {
+        render(
+            <MemoryRouter>
+                <HomeRoute />
+            </MemoryRouter>
+        );
+
+        const languages = [
+            'javascript',
+            'typescript',
+            'rust',
+            'go',
+            'python',
+            'java'
+        ]
+
+        for (const language of languages) {
+            const links = await screen.findAllByRole('link', { name: new RegExp(`${language}_`) });
+
+            expect(links).toHaveLength(2);
+            expect(links[0]).toHaveTextContent(`${language}_1`);
+            expect(links[1]).toHaveTextContent(`${language}_2`);
+            expect(links[0]).toHaveAttribute('href', `/repositories/${language}_1`);
+            expect(links[1]).toHaveAttribute('href', `/repositories/${language}_2`);
+        }
+    });
+});
